bfbeee84ac8811a604c69c7a4e0975aca2c5b8a7
quangbach <167604957+quangbach27@users.noreply.github.com>
2025-08-28 07:55:48 +0700

    initialize order service with basic configuration and structure

---
+++ src/test/java/com/polarbookshop/orderservice/book/BookClientTests.java
@@ -0,0 +1,8 @@
+package com.polarbookshop.orderservice.book;
+import okhttp3.mockwebserver.MockWebServer;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookClientTests {
+    private MockWebServer mockWebServer;
+    
+}
